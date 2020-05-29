2384a0974656b798aaa3190f70b57f7aaee7963e
Hans Bracho <hbracho@joshuatech.net>
2020-05-29 10:42:13 -0400

    Actualizacion curso servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+
 //Rutas
 import { APP_ROUTING } from './app.routes';
+
 //Servicios
+ //import { HeroesService } from './servicios/heroes.service';
+
 //Componentes 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
@@ -25,7 +29,9 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     HttpModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+   // HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
